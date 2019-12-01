a9e2ae46a77249ed33a1ee46a342769486a66f82
Montage <Montage@DJR-iMac-110-370.local>
2019-12-01 19:06:04 +0200

    first

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+
+gulp.task("sass",()=>{
+    return gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest("./css"))
+})
+gulp.task('watch',()=>{
+    gulp.watch("scss/style.scss",gulp.series('sass'))
+})
